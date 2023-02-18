f0497b2ca192be87c5232c352c493ce9e4c3f0ec
ejun <jjoonleo@gmail.com>
2023-02-19 00:08:34 +0900

    docker setting
    Co-authored-by: slash0303 <slash0303@users.noreply.github.com>

---
+++ server/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+    res.json({
+        "msg":"hello world!!",
+    });
+});
+
+app.listen(PORT,
+    console.log(`Server running in ${process.env.NODE.ENV} mode on port ${PORT}`)
+);
