c930d48e3bc194b7a54800907d017be0da179239
ejun <jjoonleo@gmail.com>
2023-02-23 20:38:42 +0900

    create foodSchema

--- server/config.js
+++ server/config.js
@@ -4,8 +4,8 @@ module.exports = {
       "mongodb+srv://ejun:j6432636@profile.vrk9e.mongodb.net/food?authSource=admin&replicaSet=atlas-ez24ee-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=true&ssl=true",
     db_schemas: [
         {
-            schemaName: "testSchema",
-            modelName: "Test",
+            schemaName: "foodSchema",
+            modelName: "Food",
         },
     ],
     route_info: [
---
+++ server/database/schema/foodSchema.js
@@ -0,0 +1,27 @@
+let Schema = {};
+
+Schema.createSchema = (mongoose) => {
+    let TicketSchema = mongoose.Schema({
+        name: {type: String, required: true},
+        spicy: {type: Boolean, required: true},//0:불닭이상 1:신라면 이상  2:매운 맛이 존재 4:매운맛이 없음
+        temperature: {type: Number, required: true},//0:HOT 1: NORMAL 2: COLD
+
+        isSweet: {type:Boolean, required: true}, 
+        isSalty: {type: Boolean, required: true},
+
+        togo: {type: Boolean, required: true},
+        delivery:{type: Boolean, required: true},
+        store:{type: Boolean, required: true},
+
+        country: {type: Number, required: true}, //0:한식 1:양식 2:중식 3:일식 4:동남아시아 5:그 외
+        cal: {type:Boolean, required: true}, //900 이상 이하
+        Main_ingredient:{type: Number, required: true}, //0:밥 1:면 2:빵 3:떡 4:육류
+        Restaurant:{type: mongoose.Schema.Types.ObjectId, ref: "Restaurant", required: true},
+    });
+
+    console.log("foodSchema is defined.");
+    
+    return TicketSchema;
+};
+
+module.exports = Schema;
