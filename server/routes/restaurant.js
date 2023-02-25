430b7ab5ac93ffed3560b75cbeac98f0f0ac64db
ejun <jjoonleo@gmail.com>
2023-02-25 17:09:59 +0900

    create router for adding new restaurant to db
    post "/restaurant"
    restaurant.js file

--- server/routes/restaurant.js
+++ server/routes/restaurant.js
@@ -6,5 +6,20 @@ router.use((req, res, next) => {
 });
 
 
+router.post("/", (req, res)=>{
+    console.log("post called");
+    console.log(req.body);
+    let db = req.app.get("database");
+    let restaurant = new db.Restaurant({name: req.body.name});
+    restaurant.save((error, _restaurant) => {
+        if (error){
+            throw error;
+        }
+        else{
+            console.log(_restaurant.name + " saved to bookstore collection.");
+            res.status(201).json({success:true});
+        }
+    });
+});
 
 module.exports = router;
