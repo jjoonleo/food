e312a0cdcde5acc3f4c26ec03a015ffa4b2c0ce5
ejun <jjoonleo@gmail.com>
2023-02-25 16:46:45 +0900

    correct foodSchema name from TicketSchema

--- server/database/schema/foodSchema.js
+++ server/database/schema/foodSchema.js
@@ -1,7 +1,7 @@
 let Schema = {};
 
 Schema.createSchema = (mongoose) => {
-    let TicketSchema = mongoose.Schema({
+    let foodSchema = mongoose.Schema({
         name: {type: String, required: true},
         spicy: {type: Boolean, required: true},//0:불닭이상 1:신라면 이상  2:매운 맛이 존재 4:매운맛이 없음
         temperature: {type: Number, required: true},//0:HOT 1: NORMAL 2: COLD
@@ -21,7 +21,7 @@ Schema.createSchema = (mongoose) => {
 
     console.log("foodSchema is defined.");
     
-    return TicketSchema;
+    return foodSchema;
 };
 
 module.exports = Schema;
