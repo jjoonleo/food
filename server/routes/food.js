cef22dd5ee50f44681324aad2f2c043a98bf96c7
ejun <jjoonleo@gmail.com>
2023-02-26 16:52:35 +0900

    create post "/food" and get "/food/all"

--- server/routes/food.js
+++ server/routes/food.js
@@ -7,4 +7,37 @@ router.use((req, res, next) => {
     next();
 });
 
-module.exports = router;module.exports = router;
+router.get("/all", (req,res)=>{
+    console.log("get /all called");
+    let db = req.app.get("database");
+    db.Food.find((error,result)=>{
+        if(error){
+            res.send(error);
+        }
+        else{
+            res.json(result);
+        }
+    })
+});
+
+router.post("/", async(req, res)=>{
+    console.log("post called");
+    console.log(req.body);
+    let db = req.app.get("database");
+
+    let restaurant = await db.Restaurant.findOne({name:req.body.restaurant})
+    console.log(restaurant);
+    req.body.restaurant = restaurant._id;
+    let food = new db.Food(req.body);
+    food.save((error, _food) => {
+        if (error){
+            throw error;
+        }
+        else{
+            console.log(_food.name + " saved to foods collection.");
+            res.status(201).json({success:true});
+        }
+    });
+});
+
+module.exports = router;
