c3a583cfc2848dd3b1ba2db48860bc0697a616d2
Ejun Park <jjoonleo@gmail.com>
2023-02-26 23:17:52 +0900

    require tryCatch

--- server/routes/food.js
+++ server/routes/food.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-let tryCatch = require("../utils/tryCatch");
+let {tryCatch} = require("../utils/tryCatch");
 
 router.use((req, res, next) => {
     console.log("food middleware is called");
