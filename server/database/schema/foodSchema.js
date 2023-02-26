778c48a323669f5dc0be4742c1c8caa35cf9f762
ejun <jjoonleo@gmail.com>
2023-02-26 14:54:48 +0900

    correct typo in foodSchema

--- server/database/schema/foodSchema.js
+++ server/database/schema/foodSchema.js
@@ -14,9 +14,9 @@ Schema.createSchema = (mongoose) => {
         store:{type: Boolean, required: true},
 
         country: {type: Number, required: true}, //0:한식 1:양식 2:중식 3:일식 4:동남아시아 5:그 외
-        cal: {type:Boolean, required: true}, //900 이상 이하
-        Main_ingredient:{type: Number, required: true}, //0:밥 1:면 2:빵 3:떡 4:육류
-        Restaurant:{type: mongoose.Schema.Types.ObjectId, ref: "Restaurant", required: true},
+        cal: {type:Boolean, required: true}, //900 이상 true
+        main_ingredient:{type: Number, required: true}, //0:밥 1:면 2:빵 3:떡 4:육류
+        restaurant:{type: mongoose.Schema.Types.ObjectId, ref: "Restaurant", required: true},
     });
 
     console.log("foodSchema is defined.");
