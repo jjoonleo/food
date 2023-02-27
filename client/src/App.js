779904d56a5b0aecc4ad71d3a6c82270c0497537
ejun <jjoonleo@gmail.com>
2023-02-28 00:58:51 +0900

    correct typo in addFood Route paramater

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/addRestaurant" element={<AddRestaurant/>} />
-          <Route path="/addFood/:resturant" element={<AddFood/>} />
+          <Route path="/addFood/:restaurant" element={<AddFood/>} />
         </Routes>
       </div>
     </Router>
